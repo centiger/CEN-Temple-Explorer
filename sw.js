867b87b138e1fbab9d56c923dd17f1800601ca2d
centiger <centiger2@gmail.com>
2026-07-07 17:09:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cen-temple-explorer-v2-tabernacle-001';
+const CACHE_NAME = 'cen-temple-explorer-v2-solomon-001';
 const CORE = [
   './','./index.html','./manifest.webmanifest',
   './assets/common/ui/app.css',
@@ -6,6 +6,8 @@ const CORE = [
   './assets/engine/explorer.js','./assets/engine/gesture.js','./assets/engine/story.js','./assets/engine/transition.js','./assets/engine/ui.js',
   './data/tabernacle.js','./data/solomon.js','./data/zerubbabel.js','./data/herod.js',
   './assets/tabernacle/scenes/01-drone.jpg','./assets/tabernacle/scenes/02-overview.jpg','./assets/tabernacle/scenes/03-courtyard.jpg','./assets/tabernacle/scenes/04-altar.jpg','./assets/tabernacle/scenes/05-laver.jpg','./assets/tabernacle/scenes/06-entrance.jpg','./assets/tabernacle/scenes/07-holy-place.jpg','./assets/tabernacle/scenes/08-ark.jpg',
+
+  './assets/solomon/scenes/01-drone.jpg','./assets/solomon/scenes/02-temple-front.jpg','./assets/solomon/scenes/03-altar.jpg','./assets/solomon/scenes/04-bronze-sea.jpg','./assets/solomon/scenes/05-entrance.jpg','./assets/solomon/scenes/06-holy-place.jpg','./assets/solomon/scenes/07-holy-of-holies.jpg',
   './assets/zerubbabel/scenes/01-drone.jpg','./assets/zerubbabel/scenes/02-temple-front.jpg','./assets/zerubbabel/scenes/03-outer-court.jpg','./assets/zerubbabel/scenes/04-altar.jpg','./assets/zerubbabel/scenes/05-laver.jpg','./assets/zerubbabel/scenes/06-entrance.jpg','./assets/zerubbabel/scenes/07-holy-place.jpg','./assets/zerubbabel/scenes/08-holy-of-holies.jpg'
 ];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(CORE)).then(()=>self.skipWaiting())));
